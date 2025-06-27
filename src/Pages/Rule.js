e36263457034b4e821f70d816a464d635a1393ab
PreneurX <preneurxteam@gmail.com>
2025-06-27 11:39:20 +0530

    Update Rule.js

--- src/Pages/Rule.js
+++ src/Pages/Rule.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Rules from '../Component/Rules'
 import Navbar from '../Component/Navbar'
 import Footer from '../Component/Footer'
+import Curousel from '../Component/Curousel';
 
 function Rule() {
   return (
     <div>
         <Navbar/>
+    <Curousel/>
         <Rules/>
         <Footer/>
     </div>
