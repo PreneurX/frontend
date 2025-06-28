bd5afa70aebd924979f231552b1a41b6dc637169
PreneurX <preneurxteam@gmail.com>
2025-06-29 00:53:46 +0530

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,61 +3,32 @@ import './Navbar.css';
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 
-
 function Navbar() {
-    // const gradientStyle = {
-    //     background: 'linear-gradient(to right, #0e1010,rgb(13, 94, 201))',
-    //     WebkitBackgroundClip: 'text',
-    //     WebkitTextFillColor: 'transparent',
-    //     fontWeight: 700,
-    //     fontSize: '1.4rem',
-    //     fontFamily: 'DM Sans, sans-serif',
-    // };
-    // const alpha ={
-    //     fontFamily: 'DM Sans, sans-serif',
-
-
-    // };
-
     const navigate = useNavigate();
-  const handleSubmit = (e) => {
-navigate("/dashboard");   
-  };
+
+    const handleSubmit = (e) => {
+        navigate("/dashboard");   
+    };
 
     return (
         <header className="navbar">
             <div className="left-section">
-                {/* Logo with gradient */}
-                {/* <span style={gradientStyle}>P
-                <span style={{...gradientStyle, fontSize: '1rem' }}>RENEUR</span>
-                <span style={alpha}>X</span>
-
-
-                </span> */}
-        <img src={logo} alt="Company Logo" className='logomy'/>
-                
+                {/* ✅ Logo wrapped in Link to homepage */}
+                <Link to="/">
+                    <img src={logo} alt="PreneurX Logo" className="logomy" />
+                </Link>
             </div>
 
             <nav className="nav-links">
-            <Link to="/">Home</Link>
-            <Link to="/">Prize Pool</Link>
-            <Link to="/rule">Rules</Link>
-            
-            <Link to="/contact">Contact Us</Link>
-
-            
-
-
-
-
-        
+                <Link to="/">Home</Link>
+                <Link to="/">Prize Pool</Link>
+                <Link to="/rule">Rules</Link>
+                <Link to="/contact">Contact Us</Link>
             </nav>
 
             <div className="right-section">
-                <button onClick={handleSubmit} className='login-btn'>Login </button>
+                <button onClick={handleSubmit} className='login-btn'>Login</button>
             </div>
-            
-
         </header>
     );
 }
