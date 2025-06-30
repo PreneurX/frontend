46c09ee64754f6d726801b53745fe8fd212d6f22
PreneurX <preneurxteam@gmail.com>
2025-06-30 13:16:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,48 @@
-import Navbar from './Component/Navbar';
-import Curousel from '../src/Component/Curousel';
-import Home from './Pages/Home';
 
 
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import Navbar from './Component/Navbar';
+import Curousel from './Component/Curousel';
+import Home from './Pages/Home';
+import Loading from '../src/Component/Loading'; // Make sure path is correct
+import Home from './Pages/Home';
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    // If already loaded
+    if (document.readyState === 'complete') {
+      handleLoad();
+    } else {
+      window.addEventListener('load', handleLoad);
+    }
+
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <Home/>
-      </div>
+      {loading && <Loading />} {/* Preloader at top */}
+      {!loading && (
+        <>
+          
+          <Home />
+        </>
+      )}
+    </div>
   );
 }
+
 export default App;
+
+
+
+
