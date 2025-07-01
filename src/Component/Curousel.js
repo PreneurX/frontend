9cd0a5890409f5869a9583f8d9e1401e513a74c7
PreneurX <preneurxteam@gmail.com>
2025-07-01 08:19:26 +0530

    Update Curousel.js

--- src/Component/Curousel.js
+++ src/Component/Curousel.js
@@ -8,17 +8,21 @@ function Curousel() {
         <hr/>
         
             <div className="mobile-carousel">
-       
-        
                 
                 <div className="carousel-option"><Link to="/">Home</Link></div>
-                <div className="carousel-option"><Link to="/">Prize Pool</Link></div>
+                <div className="carousel-option">
+                <a href="https://drive.google.com/uc?export=download&id=1AsLMl2fZvA2qurMzAg2ZH7OC0zjK5rIv"
+                target="_blank"
+                rel="noopener noreferrer"
+  >
+                Prize Pool
+               </a>
+            </div>
                 <div className="carousel-option"><Link to="/rule">Rules</Link></div>
                 
                 <div className="carousel-option"><Link to="/contact">Contact Us</Link></div>
                 <div className="carousel-option"><Link to="/">Team</Link></div>
-                {/* <div className="carousel-option"><a href="#">Trending</a></div>
-                <div className="carousel-option"><a href="#">Trending</a></div>  */}
+              
        
 
             </div>
