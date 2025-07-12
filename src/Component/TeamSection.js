1a31fbc4a6649ce76de74ea9219715407d4d7b94
PreneurX <preneurxteam@gmail.com>
2025-07-12 19:26:38 +0530

    Update TeamSection.js

--- src/Component/TeamSection.js
+++ src/Component/TeamSection.js
@@ -26,6 +26,12 @@ const teamMembers = [
     bio: "Leads financial strategy and ensures sustainable growth at PreneurX. Balances innovation with responsibility, managing budgets that fuel impactful student experiences."
   },
 {
+    name: "Yashveer Shukla",
+    role: "Co-Founder & CMO",
+    image: "/",
+    bio: "Connects PreneurX with thousands of students across India through brilliant communication skills and school outreach."
+  },
+{
     name: "Piyush Kumar Mishra",
     role: "Co-Founder & CTO",
     image: piyush,
